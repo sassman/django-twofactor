764664b485dd944c02cb5e47513bae55e34c112c
Sven Aßmann <sven.assmann@lubico.biz>
2013-01-26 17:12:00 +0100

    slighly changes on the return flow

--- django_twofactor/auth_backends.py
+++ django_twofactor/auth_backends.py
@@ -1,13 +1,18 @@
+import logging
 from django.contrib.auth.models import User
 from django.contrib.auth.backends import ModelBackend
 from django_twofactor.models import UserAuthToken
+
+log = logging.getLogger(__name__)
+
 
 class TwoFactorAuthBackend(ModelBackend):
     def authenticate(self, username=None, password=None, token=None):
         # Validate username and password first
         user_or_none = super(TwoFactorAuthBackend, self).authenticate(username, password)
-        
+
         if user_or_none and isinstance(user_or_none, User):
+            log.debug('user found')
             # Got a valid login. Now check token.
             try:
                 user_token = UserAuthToken.objects.get(user=user_or_none)
@@ -15,12 +20,10 @@ class TwoFactorAuthBackend(ModelBackend):
                 # User doesn't have two-factor authentication enabled, so
                 # just return the User object.
                 return user_or_none
-            
+
+            log.debug('check the auth token..')
             validate = user_token.check_auth_code(token)
-            if (validate == True):
-                # Auth code was valid.
-                return user_or_none
-            else:
-                # Bad auth code
-                return None
+            if not validate:
+                log.debug('bad auth token given..')
+                user_or_none = None
         return user_or_none
