642e9f07a5f1e216b318ee7a3502ad8ffb3a8075
Sven Aßmann <sven.assmann@lubico.biz>
2013-01-27 22:33:29 +0100

    first draft of setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf8 -*-
+
+from setuptools import setup
+
+version = '0.0.1'
+
+setup(
+    name='django-twofactor',
+    version=version,
+    author='Sven Aßmann',
+    author_email='sven.assmann@lubico.biz',
+    description='django auth integration with google twofactor',
+    keywords='django auth google multifactor twofactor',
+    url='https://github.com/sassman/django-twofactor',
+    license='LICENSE.txt',
+    packages=[
+        'django_twofactor',
+    ],
+    long_description=open('README.mdown').read(),
+    install_requires=[
+        'oath>=1.0',
+        'django>=1.4'
+        'pycrypto'
+    ]
+)
