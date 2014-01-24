8b6178ecc0518a9cd2244cfc2f7974af89905cb8
Sven Aßmann <sven.assmann@lubico.biz>
2014-01-24 19:54:30 +0100

    - add author Mike Tigas - add the license reference to setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ version = '0.0.1'
 setup(
     name='django-twofactor',
     version=version,
-    author='Sven Aßmann',
+    author=('Mike Tigas', 'Sven Aßmann'),
     author_email='sven.assmann@lubico.biz',
     description='django auth integration with twofactor and googleauthentication',
     keywords='django auth googleauthenticator multifactor twofactor',
     url='https://github.com/sassman/django-twofactor',
-    #license='LICENSE.txt',
+    license='LICENSE.txt',
     packages=[
         'django_twofactor',
     ],
